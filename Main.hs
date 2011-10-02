4809b223f89dd9bb7ce2aabe6e01895cb43b3b2d
Alexey Filippov <afilippov@acm.org>
2011-10-02 10:33:27 +0100

    initial structure (doubt it works) and a spellchecker example to play with

---
+++ Main.hs
@@ -0,0 +1,13 @@
+import System.Environment
+import Control.Monad
+import Data.Set
+
+main = do
+        [s] <- getArgs
+        f   <- readFile "/usr/share/dict/words"
+        g   <- readFile s
+        let dict = fromList (lines f)
+        mapM_ (spell dict) (words g)
+
+spell d w = when (w `notMember` d) (putStrLn w)
+    
