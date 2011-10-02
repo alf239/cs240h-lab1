920e7ac68a57d963c4f74b08ddcc9f4bf69a2338
Alexey Filippov <afilippov@acm.org>
2011-10-02 21:57:54 +0100

    Applied hlint suggestions

--- Main.hs
+++ Main.hs
@@ -5,15 +5,14 @@ import qualified Data.Map as Map
 main = do
         files <- getArgs
         g     <- if files == [] 
-                           then (fmap (\a->[a]) getContents)
+                           then fmap (: []) getContents
                            else mapM readFile files
         let dict = Map.toList (Map.unionsWith (+) (map (wordFreq . words) g))
         putStrLn $ histogram dict
 
 
 histogram :: (Show w, Num n) => [(w, n)] -> String
-histogram a0 = concat $ map lineTemplate a0
-               where lineTemplate (a, b) = (show a) ++ "\t" ++ (show b) ++ "\n"
+histogram = concatMap (\(a, b) -> show a ++ "\t" ++ show b ++ "\n")
 
 wordFreq :: (Ord k, Num n) => [k] -> Map.Map k n
-wordFreq list0 = foldl (\m a -> Map.insertWith' (+) a 1 m) Map.empty list0
+wordFreq = foldl (\m a -> Map.insertWith' (+) a 1 m) Map.empty
