b3237ee19cd76bc858396dad3fa222981f4c9eec
Alexey Filippov <afilippov@acm.org>
2011-10-02 19:16:25 +0100

    concat from library

--- Main.hs
+++ Main.hs
@@ -12,10 +12,7 @@ main = do
 
 
 histogram :: (Show w, Num n) => [(w, n)] -> String
-histogram a0 = hist "" a0 
-               where hist a' []    = a'
-                     hist a' (h:t) = hist (a' ++ (show w) ++ "\t" ++ (show c) ++ "\n") t
-                                     where (w, c) = h
+histogram a0 = concat $ map (\(a,b) -> (show a) ++ "\t" ++ (show b) ++ "\n") a0
 
 wordFreq :: (Ord k, Num n) => [k] -> Map.Map k n
 wordFreq list0 = foldl (\m a -> Map.insertWith' (+) a 1 m) Map.empty list0
