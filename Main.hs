72d386392f4380b3eaf5219b6a20f167877a4b90
Alexey Filippov <afilippov@acm.org>
2011-10-02 19:26:15 +0100

    A bit more readable

--- Main.hs
+++ Main.hs
@@ -5,14 +5,17 @@ import qualified Data.Map as Map
 main = do
         files <- getArgs
         g     <- if files == [] 
-                           then (fmap (\a->[a]) getContents) 
+                           then (fmap (\a->[a]) getContents)
                            else mapM readFile files
         let dict = Map.unionsWith (+) (map (wordFreq . words) g)
         putStrLn $ (histogram . Map.toList) dict
 
 
 histogram :: (Show w, Num n) => [(w, n)] -> String
-histogram a0 = concat $ map (\(a,b) -> (show a) ++ "\t" ++ (show b) ++ "\n") a0
+histogram a0 = concat $ map lineTemplate a0
+
+lineTemplate :: (Show a, Show b) => (a, b) -> String
+lineTemplate (a, b) = (show a) ++ "\t" ++ (show b) ++ "\n"
 
 wordFreq :: (Ord k, Num n) => [k] -> Map.Map k n
 wordFreq list0 = foldl (\m a -> Map.insertWith' (+) a 1 m) Map.empty list0
