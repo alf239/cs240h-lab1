74182f6c2284a4ce10ffaf7fac7bd7dcb7b931f5
Alexey Filippov <afilippov@acm.org>
2011-10-02 16:38:39 +0100

    First version of histogram (not a histogram, obviously)

--- Main.hs
+++ Main.hs
@@ -6,7 +6,12 @@ main = do
         s   <- getArgs
         g   <- mapM readFile s
         let dict = Map.unionsWith (+) (map (wordFreq . words) g)
-        print dict
+        putStrLn (histogram (Map.toList dict))
+
+histogram :: (Show w, Num n) => [(w, n)] -> String
+histogram []    = ""
+histogram (h:t) = (show a) ++ "\t" ++ (show b) ++ "\n" ++ (histogram t)
+                  where (a, b) = h
 
 wordFreq :: (Ord k, Num n) => [k] -> Map.Map k n
 wordFreq list0 = wordCount Map.empty list0
