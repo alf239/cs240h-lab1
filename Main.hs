d486731b5621f2e9dcd424e4bc04b5f8f20be146
Alexey Filippov <afilippov@acm.org>
2011-10-03 00:34:26 +0100

    Cleaned the words

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ import System.Environment
 import Control.Monad
 import qualified Data.Map as Map
 import Data.List
+import Data.Char
 import Data.Monoid (mappend)
 
 main = do
@@ -9,13 +10,15 @@ main = do
         g     <- if null files
                     then fmap (: []) getContents
                     else mapM readFile files
-        let dict = Map.toList (Map.unionsWith (+) (map (wordFreq . words) g))
+        let dict = Map.toList (wordFreq (map sToLowerCase (concatMap cleanWords g)))
         putStrLn $ histogram (sortBy bySecond dict)
 
 invert :: Ordering -> Ordering
 invert GT = LT
 invert LT = GT
 invert EQ = EQ
+
+sToLowerCase = map toLower
 
 bySecond :: (Ord a, Ord b) => (a, b) -> (a, b) -> Ordering
 bySecond (a1, b1) (a2, b2) = invert (compare b1 b2 `mappend` compare a1 a2)
@@ -25,3 +28,13 @@ histogram = concatMap (\(a, b) -> show a ++ "\t" ++ show b ++ "\n")
 
 wordFreq :: (Ord k, Num n) => [k] -> Map.Map k n
 wordFreq = foldl' (\m a -> Map.insertWith' (+) a 1 m) Map.empty
+
+isNotWordPart :: Char -> Bool
+isNotWordPart c = not (isAlpha c `or` isMark c)
+
+cleanWords          :: String -> [String]
+cleanWords s        =  case dropWhile isNotWordPart s of
+                                "" -> []
+                                s' -> w : cleanWords s''
+                                      where (w, s'') =
+                                             break isNotWordPart s'
