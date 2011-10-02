974db580d86eefa5fd63770a6042acf9285e99bc
Alexey Filippov <afilippov@acm.org>
2011-10-02 23:21:55 +0100

    Proper empty-list check; function for future sorting

--- Main.hs
+++ Main.hs
@@ -2,15 +2,18 @@ import System.Environment
 import Control.Monad
 import qualified Data.Map as Map
 import Data.List
+import Data.Monoid (mappend)
 
 main = do
         files <- getArgs
-        g     <- if files == [] 
-                           then fmap (: []) getContents
-                           else mapM readFile files
+        g     <- if null files
+                    then fmap (: []) getContents
+                    else mapM readFile files
         let dict = Map.toList (Map.unionsWith (+) (map (wordFreq . words) g))
         putStrLn $ histogram dict
 
+bySecond :: Ord a => (a, a) -> (a, a) -> Ordering
+bySecond (a1, b1) (a2, b2) = compare b1 b2 `mappend` compare a1 a2
 
 histogram :: (Show w, Num n) => [(w, n)] -> String
 histogram = concatMap (\(a, b) -> show a ++ "\t" ++ show b ++ "\n")
