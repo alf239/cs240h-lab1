febe73c587d8ca8705fe5193d281c1e0d3d01fe0
Alexey Filippov <afilippov@acm.org>
2011-10-03 08:32:22 +0100

    Code style; dropped unnecessary invert()

--- Main.hs
+++ Main.hs
@@ -13,15 +13,10 @@ main = do
         let dict = Map.toList (wordFreq (map sToLowerCase (concatMap cleanWords g)))
         putStrLn $ histogram (sortBy bySecond dict)
 
-invert :: Ordering -> Ordering
-invert GT = LT
-invert LT = GT
-invert EQ = EQ
-
 sToLowerCase = map toLower
 
 bySecond :: (Ord a, Ord b) => (a, b) -> (a, b) -> Ordering
-bySecond (a1, b1) (a2, b2) = invert (compare b1 b2 `mappend` compare a1 a2)
+bySecond (a1, b1) (a2, b2) = compare b2 b1 `mappend` compare a2 a1
 
 histogram :: (Show w, Num n) => [(w, n)] -> String
 histogram = concatMap (\(a, b) -> show a ++ "\t" ++ show b ++ "\n")
@@ -30,11 +25,11 @@ wordFreq :: (Ord k, Num n) => [k] -> Map.Map k n
 wordFreq = foldl' (\m a -> Map.insertWith' (+) a 1 m) Map.empty
 
 isNotWordPart :: Char -> Bool
-isNotWordPart c = not (isAlpha c `or` isMark c)
+isNotWordPart c = not (isAlpha c || c == '\'' || c == '-')
 
-cleanWords          :: String -> [String]
-cleanWords s        =  case dropWhile isNotWordPart s of
-                                "" -> []
-                                s' -> w : cleanWords s''
-                                      where (w, s'') =
-                                             break isNotWordPart s'
+cleanWords :: String -> [String]
+cleanWords s =  case dropWhile isNotWordPart s of
+                        "" -> []
+                        s' -> w : cleanWords s''
+                              where (w, s'') =
+                                     break isNotWordPart s'
